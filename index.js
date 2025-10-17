d7af7aeb9cccab41363af23f1fec3807a3146712
Rebka Ayele Asfaw <Rebka.Ayele@bankofabyssinia.com>
2025-10-17 09:26:18 +0300

    backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 const app = express();
 
